bfa5d04735f00378faf64c7999f6cd303a1de089
Venkatasai Pulluri <venkatasai.pulluri@experian.com>
2022-09-02 12:18:27 -0500

    first commit

---
+++ src/main/java/com/example/helloworld/Helloworld.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Helloworld {
+
+    @RequestMapping("/helloworld")
+    String home() {
+        return "Hello World - v1!";
+    }
+}
